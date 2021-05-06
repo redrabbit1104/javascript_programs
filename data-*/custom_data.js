52a313735db665e819f61c1ceed31266abef3243
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-05-07 08:08:11 +0900

    custom-data

---
+++ data-*/custom_data.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// const div = document.querySelector("div");
+// console.log(div.dataset.name);
+// test.dataset.huga = 20;
+
+const test = document.querySelector(".test");
+test.dataset.hoge = "hello";
+console.log(test);
