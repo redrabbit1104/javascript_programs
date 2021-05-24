a7f74eaa7803a1713e60d6a1fa1a091f3a622cb3
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-05-24 20:49:21 +0900

    truthy_falsy_value

--- spot_light/spot_light.js
+++ spot_light/spot_light.js
@@ -1,12 +1,13 @@
 "use strict";
 const spot = document.querySelector(".spot");
 const spotLight = (event) => {
-  spot.setAttribute(
-    "style",
-    `background:
+  console.log(event),
+    spot.setAttribute(
+      "style",
+      `background-image:
      radial-gradient(circle at ${event.clientX}px ${event.clientY}px, 
      transparent  0px, transparent 30px, rgba(0,0,0,0.9) 50px)`
-  );
+    );
 };
 
 document.addEventListener("mousemove", spotLight);
